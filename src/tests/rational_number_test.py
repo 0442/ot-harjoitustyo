a6af9e7c1bd1571b5825a9e03b299c5ea05a27cd
82746 <82746@proton.me>
2023-11-28 23:49:01 +0200

    Add more tests for rational numbers

--- src/tests/rational_number_test.py
+++ src/tests/rational_number_test.py
@@ -1,42 +1,87 @@
 import unittest
 
-from matrix.rational_number import R
+from matrix.rational_number import Rn
+
+
+class TestRatNumWithInts(unittest.TestCase):
+    def setUp(self):
+        self.a = Rn(12, 34)
+        self.b = 567
+
+    def test_addition_with_ints(self):
+        c = self.a + self.b
+        self.assertEqual(c, Rn(19290, 34))
+
+    def test_subtraction_with_ints(self):
+        c = self.a - self.b
+        self.assertEqual(c, Rn(-19266, 34))
+
+    def test_multiplication_with_ints(self):
+        c = self.a * self.b
+        self.assertEqual(c, Rn(6804, 34))
+
+    def test_division_with_ints(self):
+        c = self.a / self.b
+        self.assertEqual(c, Rn(12, 19278))
+
+    def test_comparison_with_ints(self):
+        a = Rn(123456789, 123456788)
+        b = -1234
+
+        self.assertTrue(a != b)
+        self.assertTrue(a > b)
+        self.assertFalse(a < b)
+
+        self.assertEqual(Rn(-5, -1), 5)
+        self.assertEqual(Rn(-0, -1), 0)
+
 
 class TestRatNum(unittest.TestCase):
     def setUp(self):
-        self.a = R(12, 34)
-        self.b = R(56, 78)
+        self.a = Rn(12, 34)
+        self.b = Rn(56, 78)
 
     def test_addition_of_rational_nums(self):
         c = self.a + self.b
-        self.assertEqual(c, R(710,663))
+        self.assertEqual(c, Rn(710, 663))
 
     def test_subtraction_of_rational_nums(self):
         c = self.a - self.b
-        self.assertEqual(c, R(-242,663))
+        self.assertEqual(c, Rn(-242, 663))
 
     def test_multiplication_of_rational_nums(self):
         c = self.a * self.b
-        self.assertEqual(c, R(56,221))
+        self.assertEqual(c, Rn(56, 221))
 
     def test_division_of_rational_nums(self):
         c = self.a / self.b
-        self.assertEqual(c, R(117,238))
-
-
-
-    def test_addition_of_ints(self):
-        ...
-    def test_subtraction_of_ints(self):
-        ...
-    ...
-
-
+        self.assertEqual(c, Rn(117, 238))
 
     def test_reduction_by_scaling_down(self):
-        self.assertEqual(R(500,100), 5)
-        self.assertEqual(R(5,20), R(1,4))
+        self.assertEqual(Rn(500, 100), 5)
+        self.assertEqual(Rn(5, 20), Rn(1, 4))
 
     def test_reduction_of_negative_divided_by_negative(self):
-        self.assertEqual(R(-5,-1), 5)
-        self.assertEqual(R(-12,-34), R(12,34))
+        self.assertEqual(Rn(-12, -34), Rn(12, 34))
+        self.assertEqual(Rn(-0, -1), Rn(0, 5))
+
+    def test_reduction_of_zero_nominator(self):
+        self.assertEqual(Rn(0, 112312), 0)
+        self.assertEqual(Rn(0, 112312), Rn(0, 1))
+        self.assertEqual(Rn(0, 112312), Rn(0, -1))
+
+    def test_comparison_with_rational_nums(self):
+        digits = 100
+        a = Rn(int((digits-1)*"3"+"2"), int(digits*"3"))
+        b = Rn(int((digits-1)*"3"+"4"), int(digits*"3"))
+
+        self.assertTrue(a != b)
+        self.assertTrue(a < b)
+        self.assertFalse(a > b)
+
+        self.assertTrue(a <= b)
+        self.assertFalse(a >= b)
+
+        b = a
+        self.assertTrue(a >= b)
+        self.assertTrue(a <= b)
